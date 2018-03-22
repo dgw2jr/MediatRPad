2b55839e344b52cff1703eede0af3fd4fe8edbda
Don Woodford <dgw2jr@gmail.com>
2018-03-22 13:59:51 -0500

    Allow user to set status bar label color from app config

--- MediatRPad/Controls/BottomStatusStripLabel.cs
+++ MediatRPad/Controls/BottomStatusStripLabel.cs
@@ -1,9 +1,13 @@
 ﻿using System.Windows.Forms;
+using MediatRPad.Properties;
 
 namespace MediatRPad.Controls
 {
     public sealed class BottomStatusStripLabel : ToolStripStatusLabel, IMainFormComponent
     {
-
+        public BottomStatusStripLabel()
+        {
+            ForeColor = Settings.Default.StatusBarLabelColor;
+        }
     }
 }
