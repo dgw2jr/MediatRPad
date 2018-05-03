1506ae2929b34bf51f87504db079a32ba29cb1d6
Don Woodford <dwoodford@motivaction.com>
2018-05-02 20:38:27 -0500

    Create solution

---
+++ MVCCoreTest/MVCCoreTest/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesCoreTest.Data;
+
+namespace MVCCoreTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public CarouselViewModel CarouselViewModel { get; set; }
+
+        public void OnGet()
+        {
+            CarouselViewModel = new CarouselViewModel
+            {
+                DefaultPaneId = 1,
+                CarouselPaneViewModels = new List<CarouselPaneViewModel>
+                {
+                    new CarouselPaneViewModel
+                    {
+                        Id = 1,
+                        ImageAltText = "Test",
+                        Text = "Blah blah"
+                    }
+                }
+            };
+        }
+    }
+}
